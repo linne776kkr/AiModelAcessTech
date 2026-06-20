67ec505cf80e215eafcce333986343ea0e06d7ec
linne776kkr <1445033630@qq.com>
2026-06-20 17:25:54 +0800

    调整头文件成员变量

--- sdk/include/ChatGPTProvider.h
+++ sdk/include/ChatGPTProvider.h
@@ -6,23 +6,19 @@ namespace ai_chat_sdk
     {
     public:
         // 初始化模型，传入模型配置信息
-        virtual bool initModel(const std::map<std::string, std::string> &modelConfig);
+        virtual bool initModel(const std::map<std::string, std::string> &modelConfig) override;
         // 检测模型是否有效
-        virtual bool isAvailable() const;
+        virtual bool isAvailable() const override;
         // 获取模型名称
-        virtual std::string getModelName() const;
+        virtual std::string getModelName() const override;
         // 获取模型描述信息
-        virtual std::string getModelDesc() const;
+        virtual std::string getModelDesc() const override;
         // 发送消息,全量返回
-        virtual std::string sendMessage(const std::vector<Message> &messages, const std::map<std::string, std::string> &requestParam);
+        virtual std::string sendMessage(const std::vector<Message> &messages, const std::map<std::string, std::string> &requestParam) override;
         // 发送消息，增量返回
+        // callback:第一个参数为增量参数,第二个参数为是否结束标志
         virtual std::string seneMessageStream(const std::vector<Message> &messages,
                                               const std::map<std::string, std::string> &requestParam,
-                                              std::function<void(const std::string &, bool)> callback); // callback:第一个参数为增量参数,第二个参数为是否结束标志
-
-    protected:
-        bool _isAvailable = false; // 标记模型是否有效
-        std::string _apiKey;       // API密钥
-        std::string _endpoint;     // 模型API地址
+                                              std::function<void(const std::string &, bool)> callback) override;
     };
 }
--- sdk/include/LLMProvider.h
+++ sdk/include/LLMProvider.h
@@ -21,9 +21,10 @@ namespace ai_chat_sdk
         // 发送消息,全量返回
         virtual std::string sendMessage(const std::vector<Message> &messages, const std::map<std::string, std::string> &requestParam) = 0;
         // 发送消息，增量返回
+        // callback:第一个参数为增量参数,第二个参数为是否结束标志
         virtual std::string seneMessageStream(const std::vector<Message> &messages,
                                               const std::map<std::string, std::string> &requestParam,
-                                              std::function<void(const std::string &, bool)> callback) = 0; // callback:第一个参数为增量参数,第二个参数为是否结束标志
+                                              std::function<void(const std::string &, bool)> callback) = 0;
 
     protected:
         bool _isAvailable = false; // 标记模型是否有效
